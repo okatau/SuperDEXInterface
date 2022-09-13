71ff25d2c3a5272dcf1d1304241c00855f55effb
Gleb Zverev <glebzverev2001@gmail.com>
2022-09-13 16:16:07 +0300

    fix web

--- src/index.js
+++ src/index.js
@@ -3,15 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import CrosschainRouter from './router-pages/Router/CrosschainRouter';
 
-import {BrowserRouter as Router, Switch , Route, Routes, Link} from 'react-router-dom';
-// import {HashRouter as Router, Switch , Route, Routes, Link} from 'react-router-dom';
-
+// import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-<Router>
-    <Routes>
-      <Route path="/router" element={<CrosschainRouter />} />
-    </Routes>
-  </Router>
+  <React.StrictMode>
+    <CrosschainRouter />
+  </React.StrictMode>
 );
+
+// reportWebVitals();
