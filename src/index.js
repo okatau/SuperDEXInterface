2c1bd18f3cb883f35f1be57d18614088e0495ef3
Gleb Zverev <glebzverev2001@gmail.com>
2022-09-13 13:32:33 +0300

    delete web-tsm pages

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './tsm-pages/Mint/App';
-import Valve from './tsm-pages/Valve/Valve';
-import Freezer from './tsm-pages/Freezer/Freezer';
 import CrosschainRouter from './router-pages/Router/CrosschainRouter';
-import Uniswap from './router-pages/Uniswap/Uniswap';
 
 import {BrowserRouter as Router, Switch , Route, Routes, Link} from 'react-router-dom';
 // import {HashRouter as Router, Switch , Route, Routes, Link} from 'react-router-dom';
@@ -15,14 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 <Router>
     <Routes>
-{/* TSM */}
-      <Route path="/tsm" element={<App />} />
-      <Route path= "/valve" element={<Valve />} />
-      <Route path= "/freezer" element={<Freezer />} />
-{/* Router */}
-<Route path="/router" element={<CrosschainRouter />} />
-<Route path="/uniswap" element={<Uniswap />} />
-
+      <Route path="/router" element={<CrosschainRouter />} />
     </Routes>
   </Router>
 );
